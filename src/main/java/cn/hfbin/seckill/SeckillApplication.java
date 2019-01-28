5cc20676f9f2c16db7d80895c566aee57225c07c
xu_xi <xu_xi@worksap.co.jp>
2019-01-28 14:31:44 +0800

    ???

--- src/main/java/cn/hfbin/seckill/SeckillApplication.java
+++ src/main/java/cn/hfbin/seckill/SeckillApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
+import com.google.common.base.Objects;
+
 @SpringBootApplication
 @MapperScan("cn.hfbin.seckill.dao")
 public class SeckillApplication extends SpringBootServletInitializer {
@@ -17,7 +19,7 @@ public class SeckillApplication extends SpringBootServletInitializer {
 //http://localhost:8888/page/login
 //http://localhost:8888/goods/list
 //http://localhost:8888/goods/mycat
-//18077200000 123456
+//18077200000  123456
 	//两个注意点：一：可能是用mybatis，所以用了@MapperScan导入；二：extends了SpringBootServletInitializer，待查
 	public static void main(String[] args) {
 		SpringApplication.run(SeckillApplication.class, args);
